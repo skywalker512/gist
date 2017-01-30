aae5ac58fc01e267891bcf23a1a00e1d3c6cbe6c
skywalker <houzhenhong@hotmail.com>
2017-01-30 19:48:25 +0800

    Update site.php

--- bt/site.php
+++ bt/site.php
@@ -6,6 +6,7 @@ function SetSSLConf(){
 	if($_SESSION['server_type'] == 'nginx'){
 		$nginxVersion = trim(@file_get_contents('/www/server/nginx/version.pl'));
 		$onSSL = ($nginxVersion == '1.8.1' || $nginxVersion == '-Tengine2.1.2' || $nginxVersion == '-Tengine2.2.0') ? '':"";
+                //11111111111111111111111111
 		$sslStr = "#error_page 404/404.html;{$onSSL}\n		ssl_certificate      	key/$siteName/key.csr;\n		ssl_certificate_key  key/$siteName/key.key;\n		ssl_protocols TLSv1 TLSv1.1 TLSv1.2;\n		ssl_ciphers 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';\n		ssl_prefer_server_ciphers on;\n if (\$server_port !~ 443){\n			rewrite ^/.*\$ https://\$host\$uri;\n		}\n		error_page 497  https://\$host\$uri;\n";
 		if(strpos($conf,'ssl_certificate')){
 			returnJson(true,'SSL开启成功!');
@@ -115,6 +116,7 @@ function CloseSSLConf(){
 	$file = '/www/server/'.$_SESSION['server_type'].'/conf/vhost/'.$siteName.'.conf';
 	$conf = file_get_contents($file);
 	if($_SESSION['server_type'] == 'nginx'){
+//2222222222222222222222222222222222222222222222222222222222
 		$rep = "/\s+ssl_certificate\s+.+;\s+ssl_certificate_key\s+.+;\s+ssl_protocols\s+.+;\s+ssl_ciphers\s+.+;\s+ssl_prefer_server_ciphers\s+.+;/";
 		$conf = preg_replace($rep,'',$conf);
 		$rep = "/\s+ssl\s+on;/";
